e943670552ebb16050747d67fa763446127e1945
Samanja Cartagena <c.samanja09@Pabs.local>
2025-01-15 18:40:47 -0500

    all function added

---
+++ practice_python/all.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 15 18:38:26 2025
+
+@author: c.samanja09
+"""
+
+inv =["Sword", None, None]
+
+if all(inv):
+    print('Inventory full!')    
+elif any(inv):
+    print('Inventory has items!')
+else:
+    print('Inventory empty!')
