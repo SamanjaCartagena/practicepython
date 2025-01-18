522d177a656a2b6344d9569f75a648dc6b068d00
Samanja Cartagena <c.samanja09@Pabs.local>
2025-01-17 21:14:09 -0500

    add lambda

---
+++ practice_python/lambda.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 18:28:19 2025
+
+@author: c.samanja09
+"""
+
+fruit=["apple","orange","grape","banana"]
+
+fruits=(map(lambda x:x+"s",fruit))
+
+print(list(fruits))
